afcc4853289f7a63086e57bd5ea2ebaea9562153
machuchesteven <machuchesteven@gmail.com>
2021-09-01 01:28:43 +0300

    Initialization for the BSMART Learning Management ystem Backend API using django and DRF

---
+++ lms/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HTTPResponse
+from rest_framework.views.generic import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+class Home(APIView):
+
